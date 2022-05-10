71140c13c308824cda99ce3179585b84736fb4ec
sandeep vanam <sandeepvanam58@gmail.com>
2022-05-09 23:10:21 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,12 +3,14 @@ layout: none
 ---
  window.store = {
     {% for page in site.pages %}
-      "{{ page.url | slugify }}": {
-        "title": "{{ page.name | xml_escape }}",
-        "content": {{ page.content | strip_html | strip_newlines | jsonify }},
-        "url": "{{ page.url | xml_escape }}"
-      }
-      {% unless forloop.last %},{% endunless %}
+            {% if page.name != "search.html"  and page.name != "index.js" %}
+                  "{{ page.url | slugify }}": {
+                    "title": "{{ page.name | xml_escape }}",
+                    "content": {{ page.content | strip_html | strip_newlines | jsonify }},
+                    "url": "{{ page.url | xml_escape }}"
+                  }
+                   {% unless forloop.last %},{% endunless %}
+              {% endif %}
     {% endfor %}
 
   };
